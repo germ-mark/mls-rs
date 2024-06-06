4621ec87c748a430f87ad2f202d8a9243d122874
Mark Xue <mark@germ.network>
2024-06-05 20:28:48 -0700

    directly use swift-rs as a dependency

--- mls-rs-crypto-cryptokit/build.rs
+++ mls-rs-crypto-cryptokit/build.rs
@@ -4,80 +4,12 @@
 fn main() {}
 
 #[cfg(any(target_os = "macos", target_os = "ios"))]
+use swift_rs::SwiftLinker;
+
 fn main() {
-    swift::configure();
-    swift::link_package("cryptokit-bridge", "./cryptokit-bridge/");
-}
-
-#[cfg(any(target_os = "macos", target_os = "ios"))]
-mod swift {
-    use serde::Deserialize;
-    use std::{env, process::Command};
-
-    #[derive(Debug, Deserialize)]
-    struct SwiftTargetInfo {
-        #[serde(rename = "unversionedTriple")]
-        pub unversioned_triple: String,
-        #[serde(rename = "librariesRequireRPath")]
-        pub libraries_require_rpath: bool,
-    }
-
-    #[derive(Debug, Deserialize)]
-    struct SwiftPaths {
-        #[serde(rename = "runtimeLibraryPaths")]
-        pub runtime_library_paths: Vec<String>,
-    }
-
-    #[derive(Debug, Deserialize)]
-    struct SwiftTarget {
-        pub target: SwiftTargetInfo,
-        pub paths: SwiftPaths,
-    }
-
-    fn get_target_info() -> SwiftTarget {
-        let swift_target_info_str = Command::new("swift")
-            .args(["-print-target-info"])
-            .output()
-            .unwrap()
-            .stdout;
-
-        serde_json::from_slice(&swift_target_info_str).unwrap()
-    }
-
-    pub fn configure() {
-        let swift_target_info = get_target_info();
-        if swift_target_info.target.libraries_require_rpath {
-            panic!("Libraries require RPath! Change minimum MacOS value to fix.")
-        }
-
-        swift_target_info
-            .paths
-            .runtime_library_paths
-            .iter()
-            .for_each(|path| {
-                println!("cargo:rustc-link-search=native={}", path);
-            });
-    }
-
-    pub fn link_package(package_name: &str, package_root: &str) {
-        let profile = env::var("PROFILE").unwrap();
-
-        if !Command::new("swift")
-            .args(["build", "-c", &profile])
-            .current_dir(package_root)
-            .status()
-            .unwrap()
-            .success()
-        {
-            panic!("Failed to compile swift package {}", package_name);
-        }
-
-        let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-        let swift_target_info = get_target_info();
-        println!(
-            "cargo:rustc-link-search=native={}/{}.build/{}/{}",
-            manifest_dir, package_root, swift_target_info.target.unversioned_triple, profile
-        );
-        println!("cargo:rustc-link-lib=static={}", package_name);
-    }
-}
+      SwiftLinker::new("14.0")
+         // Only if you are also targetting iOS
+         // Ensure the same minimum supported iOS version is specified as in your `Package.swift` file
+         .with_ios("17.0")
+         .with_package("cryptokit-bridge", "./cryptokit-bridge")
+         .link();
