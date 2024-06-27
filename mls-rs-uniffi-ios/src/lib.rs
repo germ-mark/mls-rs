aecde21ce0f0e8686a2acc09a9ab6fa8973f4d85
Mark Xue <mark@germ.network>
2024-06-27 10:45:18 -0700

    add group_id, to_bytes, from_bytes API annotations

--- mls-rs-uniffi-ios/src/lib.rs
+++ mls-rs-uniffi-ios/src/lib.rs
@@ -184,6 +184,22 @@ pub struct Message {
     inner: mls_rs::MlsMessage,
 }
 
+#[uniffi::export]
+impl Message {
+    #[uniffi::constructor]
+     pub fn new(bytes: &[u8]) ->  Result<Self, MlSrsError> {
+        let inner = mls_rs::MlsMessage::from_bytes(bytes)
+            .map_err(|err| err.into_any_error())?;
+        Ok( Self { inner } )
+     }
+
+     pub fn to_bytes(&self) -> Result<Vec<u8>, MlSrsError> {
+        let result = self.inner.to_bytes()
+            .map_err(|err| err.into_any_error())?;
+        Ok(result)
+     }
+}
+
 impl From<mls_rs::MlsMessage> for Message {
     fn from(inner: mls_rs::MlsMessage) -> Self {
         Self { inner }
@@ -806,6 +822,10 @@ impl Group {
     pub async fn members(&self) -> WrappedMembers {
         // let group = self.inner().await;
         self.inner().await.roster().members().into()
+    }
+
+    pub async fn group_id(&self) -> Vec<u8> {
+        self.inner().await.group_id().to_vec()
     }
 }
 
